a0f3addfbcd5035a10fe0e7ac1a1f31ddcb5a879
Volodymyr Yatskanych <volodymyr.yatskanych@itomy.ch>
2021-06-08 10:45:36 +0300

    Initial Commit

---
+++ Sources/MyPackage/FrameworkService.swift
@@ -0,0 +1,15 @@
+//
+//  FrameworkService.swift
+//  spmtest
+//
+//  Created by Volodymyr Yatskanych on 08.06.2021.
+//
+
+import Foundation
+
+public class FrameworkService {
+    
+    public static func doSomething() {
+        print("Hello, World!")
+    }
+}
